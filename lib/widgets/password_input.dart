e1ff4fe38aad464cbce160ca0d602825ce4b1e89
Abdullah Dheir <tareqm577@gmail.com>
2023-07-24 17:04:15 +0300

    Create password input widget

---
+++ lib/widgets/password_input.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PasswordInput extends StatefulWidget {
+  const PasswordInput({super.key});
+
+  @override
+  State<PasswordInput> createState() => _PasswordInputState();
+}
+
+class _PasswordInputState extends State<PasswordInput> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
