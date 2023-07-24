fb89df7bb430c2de5f8c402e0b1bf82b8bec7814
Abdullah Dheir <tareqm577@gmail.com>
2023-07-24 16:03:46 +0300

    Create login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const String screenRoute = "/login";
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
