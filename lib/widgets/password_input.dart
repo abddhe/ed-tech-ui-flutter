2f6f9eb2c29038df7d856b7b1a202dd6dc00ac68
Abdullah Dheir <tareqm577@gmail.com>
2023-07-24 17:08:00 +0300

    Finish password input widget

--- lib/widgets/password_input.dart
+++ lib/widgets/password_input.dart
@@ -1,3 +1,5 @@
+import 'package:ed_tech/constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PasswordInput extends StatefulWidget {
@@ -8,8 +10,38 @@ class PasswordInput extends StatefulWidget {
 }
 
 class _PasswordInputState extends State<PasswordInput> {
+  IconData passwordIcon = CupertinoIcons.eye;
+  bool passwordTextStatus = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextField(
+      obscureText: passwordTextStatus,
+      decoration: InputDecoration(
+        suffixIcon: GestureDetector(
+          onTap: () {
+            if (passwordTextStatus) {
+              setState(() {
+                passwordIcon = CupertinoIcons.eye_slash;
+              });
+            } else {
+              setState(() {
+                passwordIcon = CupertinoIcons.eye;
+              });
+            }
+            passwordTextStatus = !passwordTextStatus;
+          },
+          child: Icon(
+            passwordIcon,
+            size: 30.0,
+          ),
+        ),
+        hintText: "Password",
+        border: OutlineInputBorder(
+          borderSide: const BorderSide(color: kDarkColor, width: 2),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+      ),
+    );
   }
 }
