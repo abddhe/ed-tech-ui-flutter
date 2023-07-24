d87c592f6284cb58d0b50982e329239ca9525a86
Abdullah Dheir <tareqm577@gmail.com>
2023-07-24 20:21:36 +0300

    Create model course

---
+++ lib/models/course.dart
@@ -0,0 +1,19 @@
+class Course {
+  final String title;
+  final String imageURL;
+  final String shortDescription;
+  final String description;
+  final double price;
+  final String time;
+  final String category;
+
+  Course( {
+    required this.title,
+    required this.imageURL,
+    required this.shortDescription,
+    required this.description,
+    required this.price,
+    required this.category,
+    required this.time,
+  });
+}
