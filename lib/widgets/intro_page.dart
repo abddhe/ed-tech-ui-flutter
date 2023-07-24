5a4f4ce73c4cbeb1256c2e17cc88e938cf707397
Abdullah Dheir <tareqm577@gmail.com>
2023-07-24 11:47:37 +0300

    Create intro pages widget

---
+++ lib/widgets/intro_page.dart
@@ -0,0 +1,99 @@
+import 'package:ed_tech/constants.dart';
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  final String image;
+  final String title;
+  final String body;
+  final VoidCallback onPressed;
+  final VoidCallback skip;
+
+  const IntroPage({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.body,
+    required this.onPressed,
+    required this.skip,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      color: kWhiteColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          GestureDetector(
+            onTap: skip,
+            child: SizedBox(
+              width: double.infinity,
+              child: Text(
+                "Skip",
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: kDarkGreyColor,
+                    ),
+                textAlign: TextAlign.end,
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Image.asset(
+                image,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                width: 230,
+                child: Text(
+                 title ,
+                  style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                        color: kDarkColor,
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                width: 350,
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  body,
+                  style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                        color: kDarkGreyColor,
+                      ),
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+              onPressed: onPressed,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  "Next",
+                  style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                        color: kWhiteColor,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
