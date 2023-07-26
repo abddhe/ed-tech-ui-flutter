baae492b768ddde1156241f2679aae61c6a75364
Abdullah Dheir <tareqm577@gmail.com>
2023-07-26 10:54:25 +0300

    Create profile page widget

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,17 @@
+import 'package:ed_tech/constants.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(kDefaultPadding),
+        ),
+      ),
+    );
+  }
+}
