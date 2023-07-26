53dcddeb6777264c38a5a6155c1592d376f60015
Abdullah Dheir <tareqm577@gmail.com>
2023-07-26 10:13:56 +0300

    Extract course page to separate widget && Add tabBar to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,5 @@
-import 'package:ed_tech/app_data.dart';
 import 'package:ed_tech/constants.dart';
-import 'package:ed_tech/widgets/badge_widget.dart';
-import 'package:ed_tech/widgets/course_card.dart';
-import 'package:ed_tech/widgets/search_input.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:ed_tech/pages/course_page.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,127 +12,67 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _currentIndex = 0;
+  final List<Widget> _tabBarChildren = const [
+    CoursePage(),
+    Text("Profile"),
+    Text("Setting"),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
-              child: ListTile(
-                title: Text(
-                  "Hello,",
-                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                        color: kDarkColor,
-                      ),
-                ),
-                subtitle: Text(
-                  "Abdullah Dheir",
-                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                        color: kDarkColor,
-                        fontWeight: FontWeight.w900,
-                      ),
-                ),
-                trailing: Container(
-                  width: 50,
-                  height: 50,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: kGreyColor,
-                    ),
-                  ),
-                  child: const Icon(
-                    CupertinoIcons.bell,
-                    color: kDarkColor,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(right: 15.0, left: 15.0),
-              child: Column(
-                children: [
-                  SearchInput(
-                    text: "Search course",
-                    onPressed: () {},
-                  ),
-                  const SizedBox(
-                    height: 15.0,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "Category:",
-                        style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                              fontSize: 17.0,
-                            ),
-                      ),
-                      const Expanded(
-                        child: SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            children: [
-                              BadgeWidget(text: "#CSS"),
-                              BadgeWidget(text: "#UX"),
-                              BadgeWidget(text: "#Swift"),
-                              BadgeWidget(text: "#UI"),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: kDefaultPadding,
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: kDefaultPadding,
-                ),
-                itemBuilder: (context, index) =>
-                    CourseCard(course: coursesList[index]),
-                itemCount: coursesList.length,
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Container(
-        decoration: const BoxDecoration(
-          color: kWhiteColor,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20.0),
-            topRight: Radius.circular(20.0),
+    return DefaultTabController(
+      initialIndex: 0,
+      length: _tabBarChildren.length,
+      child: Scaffold(
+        body: SafeArea(
+          child: TabBarView(
+            children: _tabBarChildren,
           ),
         ),
-        child: BottomNavigationBar(
-          backgroundColor: kWhiteColor,
-
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.bookmark_add), label: "Course"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.settings), label: "Settings"),
-          ],
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
+        bottomSheet: Container(
+          decoration: const BoxDecoration(
+            color: kWhiteColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
+            ),
+          ),
+          child: TabBar(
+            indicatorColor: Theme.of(context).colorScheme.primary,
+            unselectedLabelColor: kDarkColor,
+            labelColor: kPrimaryColor,
+            tabs: const [
+              Tab(
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.bookmark_add,
+                    ),
+                    Text(
+                      'Courses',
+                    ),
+                  ],
+                ),
+              ),
+              Tab(
+                child: Column(
+                  children: [
+                    Icon(Icons.person),
+                    Text('Profile'),
+                  ],
+                ),
+              ),
+              Tab(
+                child: Column(
+                  children: [
+                    Icon(Icons.settings),
+                    Text('Settings'),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-
       ),
     );
   }
