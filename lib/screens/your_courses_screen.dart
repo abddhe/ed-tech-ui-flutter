049d117d4b34d7d52e2c61cba4e6a97509522689
Abdullah Dheir <tareqm577@gmail.com>
2023-07-28 10:48:41 +0300

    Finish your courses screen

--- lib/screens/your_courses_screen.dart
+++ lib/screens/your_courses_screen.dart
@@ -1,17 +1,28 @@
+import 'package:ed_tech/app_data.dart';
 import 'package:ed_tech/constants.dart';
+import 'package:ed_tech/widgets/appbar.dart';
+import 'package:ed_tech/widgets/course_card.dart';
 import 'package:flutter/material.dart';
 
 class YourCoursesScreen extends StatelessWidget {
   static const String screenRoute = "/your-courses";
+
   const YourCoursesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBarWidget(title: "Your Courses"),
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.all(kDefaultPadding),
-
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                ...currentUser.courses.map((e) => CourseCard(course: e)),
+              ],
+            ),
+          ),
         ),
       ),
     );
