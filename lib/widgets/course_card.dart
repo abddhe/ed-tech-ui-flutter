9587c69fba3bca6df02d6bb749d8f1cb73629fc7
Abdullah Dheir <tareqm577@gmail.com>
2023-07-28 08:55:55 +0300

    Make course clickable

--- lib/widgets/course_card.dart
+++ lib/widgets/course_card.dart
@@ -5,86 +5,90 @@ import 'package:flutter/material.dart';
 
 class CourseCard extends StatelessWidget {
   final Course course;
-  const CourseCard({super.key, required this.course});
+  final VoidCallback? onPressed;
+  const CourseCard({super.key, required this.course, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-        side: const  BorderSide(
-          width: 2.0,
-          color: kGreyColor,
-        ),
-      ),
-      elevation: 1,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                color: kErrorColor.withAlpha(5),
-                width: double.infinity,
-                height: 250.0,
-                child: Image.asset(
-                  "assets/images/UI.png",
-                ),
-              ),
-               const Positioned(
-                bottom: 20,
-                right: 20,
-                child: BadgeWidget(
-                  text: "\$ 50",
-                ),
-              ),
-            ],
+    return GestureDetector(
+      onTap: onPressed,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          side: const  BorderSide(
+            width: 2.0,
+            color: kGreyColor,
           ),
-          Container(
-            color: kWhiteColor,
-            padding: const EdgeInsets.all(kDefaultPadding),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+        ),
+        elevation: 1,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              alignment: Alignment.center,
               children: [
-                Text(
-                  "3 h 30 min",
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleSmall!
-                      .copyWith(
-                    color: kSuccessColor,
+                Container(
+                  color: kErrorColor.withAlpha(5),
+                  width: double.infinity,
+                  height: 250.0,
+                  child: Image.asset(
+                    "assets/images/UI.png",
                   ),
                 ),
-                const SizedBox(
-                  height: kDefaultPadding,
-                ),
-                Text(
-                  "UI",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineSmall!
-                      .copyWith(
-                    color: kDarkColor,
-                    fontWeight: FontWeight.w900,
-                  ),
-                ),
-                const SizedBox(
-                  height: kDefaultPadding,
-                ),
-                Text(
-                  "Advanced mobile Interface design",
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleSmall!
-                      .copyWith(
-                    color: kDarkColor,
+                 const Positioned(
+                  bottom: 20,
+                  right: 20,
+                  child: BadgeWidget(
+                    text: "\$ 50",
                   ),
                 ),
               ],
             ),
-          )
-        ],
+            Container(
+              color: kWhiteColor,
+              padding: const EdgeInsets.all(kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "3 h 30 min",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleSmall!
+                        .copyWith(
+                      color: kSuccessColor,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: kDefaultPadding,
+                  ),
+                  Text(
+                    "UI",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall!
+                        .copyWith(
+                      color: kDarkColor,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: kDefaultPadding,
+                  ),
+                  Text(
+                    "Advanced mobile Interface design",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleSmall!
+                        .copyWith(
+                      color: kDarkColor,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
