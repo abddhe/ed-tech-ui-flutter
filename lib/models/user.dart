c85e9a8ee595961fbdd5c1b54f2b71f86394823b
Abdullah Dheir <tareqm577@gmail.com>
2023-07-28 08:29:25 +0300

    Update attribute of user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,11 +3,11 @@ import 'package:ed_tech/models/course.dart';
 class User {
   final String name;
   final String email;
-  final List<Course> course;
+  final List<Course> courses;
 
   const User({
     required this.name,
     required this.email,
-    required this.course,
+    required this.courses,
   });
 }
